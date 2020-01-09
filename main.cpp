825aebd206a404e5b44122475a7b2bd9b9ebfbd4
cmput274 <cmput274@ualberta.ca>
2020-01-09 09:32:44 -0700

    Fixed reading from Serial

--- main.cpp
+++ main.cpp
@@ -1,27 +1,45 @@
 #include <Arduino.h>
 
-int main() {
+void setup() {
     init();
     Serial.begin(9600);
-
-
-    pinMode(13, OUTPUT);
-    int numberTimes = 100;
-    int x = 0;
-
-    while(1==1){
-        char input = Serial.read();
-        if (input == 'T') {
-            String strToBeOutputted = "{";
-            strToBeOutputted += "'Value1':" + String(x) + "," + "\"Value2\":" + String(x +1) + "}";
-            Serial.println(strToBeOutputted);
-            Serial.println("NOTICED");
-            x++;
-        }
-       
-    }
-
-    Serial.println("end");
-
-    return 0;
 }
+
+
+int main(){
+  setup();
+  int x = 0;
+
+  while (1 == 1) {
+    if (Serial.available() > 0) {
+      char input = Serial.read();
+
+      if (char(input) == 'T'){
+        String strToBeOutputted = "{\"Value1\":" + String(x) + ",\"Value2\":" + String(x +1) + "}";
+        Serial.println(strToBeOutputted);
+        x++;
+      }
+    }
+  }
+  Serial.println("end");
+  return 0;
+}
+
+// void setup(){
+//   init();
+//   Serial.begin(9600);
+// }
+//
+// int main() {
+//   setup();
+//   int x = 0;
+//
+//   while (1) {
+//     Serial.print("THIS");
+//     Serial.println(x);
+//     x++;
+//     delay(1000);
+//   }
+//
+//   return(0);
+// }
