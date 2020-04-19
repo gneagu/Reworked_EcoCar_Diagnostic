7bf0bd8c414300ef2cf59ed87cc533a714581eb9
gneagu <gneagu@users.noreply.github.com>
2020-04-19 12:19:12 -0600

    Update coms_hub.py
    Made some changes. Pushing to switch machine

--- coms_hub.py
+++ coms_hub.py
@@ -20,18 +20,18 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.set_pushButton_2.clicked.connect(self.set_data_view_variables)
-        self.ui.refresh_pushButton.clicked.connect(self.appinit)
+        # self.ui.refresh_pushButton.clicked.connect(self.appinit)
 
         # This searches active com ports, and adds them to the comboBox
         self.set_port_comboBox_selections()
 
-    # Initializes seperate thread.
-    def appinit(self):
-        thread = Worker(connection)
-        self.connect(thread, thread.signal, self.testfunc)
-
-        # Get dict of values and data types from coms board
-        thread.start()
+    # # Initializes seperate thread.
+    # def appinit(self):
+    #     thread = Worker(connection)
+    #     self.connect(thread, thread.signal, self.testfunc)
+    #
+    #     # Get dict of values and data types from coms board
+    #     thread.start()
 
     # Taking user selection, opens a conenction on specified port.
     def port_connect(self):
@@ -75,38 +75,13 @@ class MainWindow(QtWidgets.QMainWindow):
     def set_data_view_variables(self):
         self.connection = self.port_connect()
         self.dict_value_type = self.get_value_name_dict(self.connection)
+        print(self.dict_value_type)
 
         self.thread = DataCollectionThread(self.connection, self.dict_value_type)
         self.thread.new_data_dict.connect(self.update_data_view)
         self.thread.start()
 
 
-
-
-
-    # Initializes seperate thread.
-    # def appinit2(self):
-    #     self.ui.tableWidget.insertColumn(0)
-    #     self.ui.tableWidget.insertColumn(1)
-    #     self.ui.tableWidget.insertColumn(2)
-    #
-        # Given dict with variables, query board for value and upgate gui
-        #
-        # for i in range(self.numOfVars):
-        #     #Get value of variable
-        #     name = "X"
-        #
-        #     self.ui.tableWidget.insertRow(i)
-        #
-        #     #Create text name
-        #     self.ui.tableWidget.setItem(i, 0,QtGui.QTableWidgetItem(name))
-        #
-        #     #Create Button to push to tableWidget
-        #     self.buttons.append(QtGui.QPushButton(self.ui.tableWidget))
-        #     self.buttons[i].setText("Graph".format(i))
-        #
-        #     #Set cell as text, and button
-        #     self.ui.tableWidget.setCellWidget(i, 2, self.buttons[i])
 
 
     # Find and add active COM ports to the gui combobox.
@@ -135,8 +110,8 @@ class MainWindow(QtWidgets.QMainWindow):
             for port in list_of_ports:
                 self.ui.com_port_comboBox.addItem(port)
 
-    def testfunc(self, sigstr):
-        return(sigstr)
+    # def testfunc(self, sigstr):
+    #     return(sigstr)
 
     def update_data_view(self, data):
         print("FInally here")
@@ -160,13 +135,17 @@ class DataCollectionThread(QThread):
 
     #Self.emit will emit data back to main thread.
     def run(self):
-        print("Her")
+        for i in range(1000):
+            print("HERE NUMBER: {}".format(i))
+            print("Her")
+            # time.sleep(1)
+            self.new_data_dict.emit({})
+
         print(self.connection)
 
-        self.new_data_dict.emit({})
 
-    def __del__(self):
-        self.wait()
+    # def __del__(self):
+    #     self.wait()
 
     # Function to kill a thread
     # https://stackoverflow.com/questions/51135444/how-to-kill-a-running-thread
@@ -180,3 +159,33 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
+
+
+
+
+
+
+    # Initializes seperate thread.
+    # def appinit2(self):
+    #     self.ui.tableWidget.insertColumn(0)
+    #     self.ui.tableWidget.insertColumn(1)
+    #     self.ui.tableWidget.insertColumn(2)
+    #
+        # Given dict with variables, query board for value and upgate gui
+        #
+        # for i in range(self.numOfVars):
+        #     #Get value of variable
+        #     name = "X"
+        #
+        #     self.ui.tableWidget.insertRow(i)
+        #
+        #     #Create text name
+        #     self.ui.tableWidget.setItem(i, 0,QtGui.QTableWidgetItem(name))
+        #
+        #     #Create Button to push to tableWidget
+        #     self.buttons.append(QtGui.QPushButton(self.ui.tableWidget))
+        #     self.buttons[i].setText("Graph".format(i))
+        #
+        #     #Set cell as text, and button
+        #     self.ui.tableWidget.setCellWidget(i, 2, self.buttons[i])
