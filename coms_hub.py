cb43b2ba8b8616a5225a79fa911030fe15218da9
gneagu <gneagu@users.noreply.github.com>
2020-05-05 00:40:00 -0600

    Update coms_hub.py
    Added ability to query every x seconds based on ui time delay settings

--- coms_hub.py
+++ coms_hub.py
@@ -16,11 +16,12 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.numOfVars = 0
         self.buttons = []
+        self.time_delay = 0.2
         self.ui = mainUI2.Ui_Dialog()
         self.ui.setupUi(self)
 
         self.ui.set_pushButton_2.clicked.connect(self.set_data_view_variables)
-        # self.ui.refresh_pushButton.clicked.connect(self.appinit)
+        self.ui.refresh_pushButton.clicked.connect(self.kill_tread)
 
         # This searches active com ports, and adds them to the comboBox
         self.set_port_comboBox_selections()
@@ -32,14 +33,40 @@ class MainWindow(QtWidgets.QMainWindow):
     #
     #     # Get dict of values and data types from coms board
     #     thread.start()
+    def kill_tread(self):
+        self.thread.stop()
 
     # Taking user selection, opens a conenction on specified port.
     def port_connect(self):
+        self.time_delay = self.ui.time_spinBox.value() / 1000
         COM_port = self.ui.com_port_comboBox.currentText()
         baud_rate = self.ui.baud_rate_lineEdit.text()
         portConnection = serial.Serial(COM_port, baud_rate, bytesize=8, parity='N', stopbits=1)
         return(portConnection)
 
+    def add_columns(self, numOfVars):
+        #Add columns here to table.
+        self.ui.tableWidget.insertColumn(0)
+        self.ui.tableWidget.insertColumn(1)
+        self.ui.tableWidget.insertColumn(2)
+
+        Given dict with variables, query board for value and upgate gui
+
+        for i in range(self.numOfVars):
+            #Get value of variable
+            name = "X"
+
+            self.ui.tableWidget.insertRow(i)
+
+            #Create text name
+            self.ui.tableWidget.setItem(i, 0,QtGui.QTableWidgetItem(name))
+
+            #Create Button to push to tableWidget
+            self.buttons.append(QtGui.QPushButton(self.ui.tableWidget))
+            self.buttons[i].setText("Graph".format(i))
+
+            #Set cell as text, and button
+            self.ui.tableWidget.setCellWidget(i, 2, self.buttons[i])
 
     def get_value_name_dict(self, serial):
         ser = serial
@@ -51,6 +78,9 @@ class MainWindow(QtWidgets.QMainWindow):
         # Number of variables is returned as a bit array. Ex// b'VAL *VCOUNT 11\n'
         numOfVars = ser.readline().decode(encoding='ascii').split(" ")[-1]
         self.numOfVars = numOfVars
+
+        #Set the columns here.
+        self.add_columns(numOfVars)
 
         # Sending "GET *VN#x" where x is number of variable returns variable name and var type.
         for i in range(int(numOfVars)):
@@ -72,7 +102,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
         return dict_value_type
 
-    # Recieves a dict with new data from coms board, and sets appropriate columns in gui.
+    # Recieves a dict with new data from coms board, and sends to worker thread.
+    # Then starts worker thread.
     def set_data_view_variables(self):
         self.connection = self.port_connect()
         self.dict_value_type = self.get_value_name_dict(self.connection)
@@ -83,7 +114,8 @@ class MainWindow(QtWidgets.QMainWindow):
         #Launch seperate thread to get variable from coms hub.
         self.thread = DataCollectionThread()
         self.thread.new_data_dict.connect(self.update_data_view)
-        self.thread.setup(self.dict_value_type, self.connection)
+        # https://stackoverflow.com/questions/45668961/send-data-to-qthread-when-in-have-changes-in-gui-windows-pyqt5
+        self.thread.setup(self.dict_value_type, self.connection, self.time_delay)
         self.thread.start()
 
 
@@ -115,9 +147,8 @@ class MainWindow(QtWidgets.QMainWindow):
             for port in list_of_ports:
                 self.ui.com_port_comboBox.addItem(port)
 
-    # def testfunc(self, sigstr):
-    #     return(sigstr)
-
+    # Emmited data from DataCollectionThread come here.
+    # This function updates the values in the main window.
     def update_data_view(self, data):
         print("FInally here")
         print(data)
@@ -140,31 +171,35 @@ class DataCollectionThread(QThread):
         self.connection = 0
         self.value_dict = {}
 
-    def setup(self, dict_value_names, serial_con):
+    def setup(self, dict_value_names, serial_con, time_delay):
         self.connection = serial_con
         self.value_dict = dict_value_names
+        self.time_delay = time_delay
 
-    #Self.emit will emit data back to main thread.
+    # This is the main function of the thread. Purpose is to query coms hub
+    # for variable from dictionary of value names and types.
     def run(self):
 
         print(self.value_dict)
+        values_read = {}
 
-        for i in self.value_dict:
-            bitString = "GET {}".format(i)
-            self.connection.write(bitString.encode(encoding='ascii'))
-            print("HERE NUMBER: {}".format(i))
-            print("Her")
-            # time.sleep(1)
-            valueList = self.connection.readline().decode(encoding='ascii')
-            print(valueList)
+        #From the dictionary, get value name, and expected value type.
+        while True:
+            for name, type in self.value_dict.items():
 
-        self.new_data_dict.emit({})
+                #Command to get variable
+                bitString = "GET {}".format(name)
+                self.connection.write(bitString.encode(encoding='ascii'))
 
-        print(self.connection)
+                # Read value from Coms hub
+                value = self.connection.readline().decode(encoding='ascii').split(" ")[-1]
+                if type == 'F':
+                    values_read[name] = float(value)
+                else:
+                    values_read[name] = value
 
-
-    # def __del__(self):
-    #     self.wait()
+            self.new_data_dict.emit(values_read)
+            time.sleep(self.time_delay)
 
     # Function to kill a thread
     # https://stackoverflow.com/questions/51135444/how-to-kill-a-running-thread
@@ -187,10 +222,10 @@ if __name__ == "__main__":
 
     # Initializes seperate thread.
     # def appinit2(self):
-    #     self.ui.tableWidget.insertColumn(0)
-    #     self.ui.tableWidget.insertColumn(1)
-    #     self.ui.tableWidget.insertColumn(2)
-    #
+        # self.ui.tableWidget.insertColumn(0)
+        # self.ui.tableWidget.insertColumn(1)
+        # self.ui.tableWidget.insertColumn(2)
+        #
         # Given dict with variables, query board for value and upgate gui
         #
         # for i in range(self.numOfVars):
