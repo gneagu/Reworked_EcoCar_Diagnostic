fef01a34f2c7fbc6d17900954e57438c4124e316
gneagu <gneagu@users.noreply.github.com>
2020-05-20 11:10:11 -0600

    Added debug mode
    Added debug mode so that project can be worked on without coms hub. Just set debug value in line 12

--- coms_hub.py
+++ coms_hub.py
@@ -7,6 +7,7 @@ from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtCore import QThread, pyqtSignal
 from gui import mainUI2, trial
 import sys
+import random
 
 debug = 0
 
@@ -55,12 +56,16 @@ class MainWindow(QtWidgets.QMainWindow):
 
         #TODO: Change this. (opening connection on same port twice crashes program)
         #But we can change com ports.
-        if not self.connection:
-            print("MAKING A NEW CONNECTION")
+        if debug == 0:
 
-            portConnection = serial.Serial(COM_port, baud_rate, bytesize=8, parity='N', stopbits=1)
-            self.connection = portConnection
+            if not self.connection:
+                print("MAKING A NEW CONNECTION")
 
+                portConnection = serial.Serial(COM_port, baud_rate, bytesize=8, parity='N', stopbits=1)
+                self.connection = portConnection
+
+        else:
+            pass
 
     # Remove rows and columns. (Needs to be reversed since removing column at start
     # remaps proceeding)
@@ -127,39 +132,44 @@ class MainWindow(QtWidgets.QMainWindow):
         print(serial)
         print(self.connection)
 
-        # Wipe serial connection buffer
-        ser.flushInput()
-        ser.flushOutput()
+        if debug == 0:
 
-        # Send command to get number of variables from the board.
-        ser.write(b'GET *VCOUNT\n')
+            # Wipe serial connection buffer
+            ser.flushInput()
+            ser.flushOutput()
 
-        # Number of variables is returned as a bit array. Ex// b'VAL *VCOUNT 11\n'
-        numOfVars = ser.readline().decode(encoding='ascii').split(" ")[-1]
-        self.numOfVars = numOfVars
+            # Send command to get number of variables from the board.
+            ser.write(b'GET *VCOUNT\n')
 
-        # #Set the columns here.
-        # self.add_columns(numOfVars)
+            # Number of variables is returned as a bit array. Ex// b'VAL *VCOUNT 11\n'
+            numOfVars = ser.readline().decode(encoding='ascii').split(" ")[-1]
+            self.numOfVars = numOfVars
 
-        # Sending "GET *VN#x" where x is number of variable returns variable name and var type.
-        for i in range(int(numOfVars)):
-            #Create string and convert to bit array.
-            bitString = "GET *VN#{}\n".format(i)
-            ser.write(bitString.encode(encoding='ascii'))
-            time.sleep(0.1)
+            # #Set the columns here.
+            # self.add_columns(numOfVars)
 
-            # Value list returns as ["command sent", "variable type", "variable name"]
-            # Variable name and type returned Ex//
-            valueList = ser.readline().decode(encoding='ascii').split(" ")
+            # Sending "GET *VN#x" where x is number of variable returns variable name and var type.
+            for i in range(int(numOfVars)):
+                #Create string and convert to bit array.
+                bitString = "GET *VN#{}\n".format(i)
+                ser.write(bitString.encode(encoding='ascii'))
+                time.sleep(0.1)
 
-            # Ex// ["VAL", "*VN#1:F", "MOT_I"]
-            valueName = valueList[-1]
-            valueType = valueList[1].split(":")[-1]
+                # Value list returns as ["command sent", "variable type", "variable name"]
+                # Variable name and type returned Ex//
+                valueList = ser.readline().decode(encoding='ascii').split(" ")
 
-            # As of Python 3.7, dicts are ordered.
-            dict_value_type[valueName] = valueType
+                # Ex// ["VAL", "*VN#1:F", "MOT_I"]
+                valueName = valueList[-1]
+                valueType = valueList[1].split(":")[-1]
 
+                # As of Python 3.7, dicts are ordered.
+                dict_value_type[valueName] = valueType
 
+        else:
+            self.numOfVars = 6
+            dict_value_type = generate_random_data()
+            print(dict_value_type)
 
         return dict_value_type
 
@@ -179,6 +189,9 @@ class MainWindow(QtWidgets.QMainWindow):
         # self.connection = self.port_connect()
         self.port_connect()
         self.dict_value_type = self.get_value_name_dict(self.connection)
+
+        print("Here")
+        print(self.dict_value_type)
 
         #Set the columns here.
         self.add_columns(self.numOfVars)
@@ -201,28 +214,34 @@ class MainWindow(QtWidgets.QMainWindow):
     def set_port_comboBox_selections(self):
         list_of_ports = []
 
-        if sys.platform.startswith('linux'):
-            ports = ["/dev/ttyACM{}".format(i) for i in range(20)]
-        elif sys.platform.startswith('win'):
-            ports = ['COM{}'.format(i + 1) for i in range(255)]
+        if debug == 0:
 
-        #See if possible to open connection on port (should only open if theres an active device)
-        for port in ports:
-            try:
-                portConnection = serial.Serial(port)
-                print("Connect to port: {}".format(port))
-                list_of_ports.append(port)
-            except:
-                pass
+            if sys.platform.startswith('linux'):
+                ports = ["/dev/ttyACM{}".format(i) for i in range(20)]
+            elif sys.platform.startswith('win'):
+                ports = ['COM{}'.format(i + 1) for i in range(255)]
 
-        #Check if any ports are available.
-        if len(list_of_ports) == 0:
-            self.ui.com_port_comboBox.addItem("No Ports Found")
-            self.ui.set_pushButton_2.setEnabled(False);
+            #See if possible to open connection on port (should only open if theres an active device)
+            for port in ports:
+                try:
+                    portConnection = serial.Serial(port)
+                    print("Connect to port: {}".format(port))
+                    list_of_ports.append(port)
+                except:
+                    pass
+
+            #Check if any ports are available.
+            if len(list_of_ports) == 0:
+                self.ui.com_port_comboBox.addItem("No Ports Found")
+                self.ui.set_pushButton_2.setEnabled(False);
+            else:
+                self.ui.set_pushButton_2.setEnabled(True);
+                for port in list_of_ports:
+                    self.ui.com_port_comboBox.addItem(port)
         else:
             self.ui.set_pushButton_2.setEnabled(True);
-            for port in list_of_ports:
-                self.ui.com_port_comboBox.addItem(port)
+            self.ui.com_port_comboBox.addItem("DEBUG")
+
 
     # Emmited data from DataCollectionThread come here.
     # This function updates the values in the main window.
@@ -268,33 +287,54 @@ class DataCollectionThread(QThread):
         print(self.value_dict)
         values_read = {}
 
-        #From the dictionary, get value name, and expected value type.
-        while True:
-            for name, type in self.value_dict.items():
+        # This is the main function of the thread. Purpose is to query coms hub
+        # for variable from dictionary of value names and types.
+ 
+        if debug == 0:
 
-                #Command to get variable
-                bitString = "GET {}".format(name)
-                self.connection.write(bitString.encode(encoding='ascii'))
+            #From the dictionary, get value name, and expected value type.
+            while True:
+                for name, type in self.value_dict.items():
 
-                try:
-                    # Read value from Coms hub
-                    value = self.connection.readline().decode(encoding='ascii').split(" ")[-1]
-                    if type == 'F':
-                        values_read[name] = float(value)
-                    else:
-                        values_read[name] = value
-                except: #Sometimes get here when reset 
-                    print("Error, at reading data")
-                    pass
+                    #Command to get variable
+                    bitString = "GET {}".format(name)
+                    self.connection.write(bitString.encode(encoding='ascii'))
 
-            self.new_data_dict.emit(values_read)
-            time.sleep(self.time_delay)
+                    try:
+                        # Read value from Coms hub
+                        value = self.connection.readline().decode(encoding='ascii').split(" ")[-1]
+                        if type == 'F':
+                            values_read[name] = float(value)
+                        else:
+                            values_read[name] = value
+                    except: #Sometimes get here when reset 
+                        print("Error, at reading data")
+                        pass
+        
+                self.new_data_dict.emit(values_read)
+                time.sleep(self.time_delay)
+
+        else:
+            while True:
+                values_read = generate_random_data()
+                self.new_data_dict.emit(values_read)
+                time.sleep(self.time_delay)
+
 
     # Function to kill a thread
     # https://stackoverflow.com/questions/51135444/how-to-kill-a-running-thread
     def stop(self):
         self.threadactive = False
         self.wait()
+
+def generate_random_data():
+    values = ['data1','data2','data3','data4','data5','data6']
+    data_dict = {}
+
+    for i in values:
+        data_dict[i] = random.randint(1, 10)
+
+    return(data_dict)
 
 # Make another worker here for the graph screen. Can connect the the function that gets coms data to multiple functions. 
 # https://stackoverflow.com/questions/10653704/pyqt-connect-signal-to-multiple-slot
